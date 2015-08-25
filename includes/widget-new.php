420bfa046a9025f0204727dd3d5ee59dccd583d6
SamMorgan <sam@svmorgan.com>
2015-08-25 12:07:17 +1000

    Max with to times thumbs, 5em h for article thumbs

--- includes/widget-new.php
+++ includes/widget-new.php
@@ -4,12 +4,12 @@
 	echo '<svg class="moon"><use xlink:href="#moon-'.$moon_phase.'" /></use></svg>';
 	echo '<span>';
 	if($moon_phase <= 1) echo 'New';
-	if($moon_phase == 2) echo 'Waxing Crescent';
-	if($moon_phase == 3) echo 'First Quarter';
-	if($moon_phase == 4) echo 'Waxing Gibbous';
+	if($moon_phase == 2) echo 'Waxing<br>Crescent';
+	if($moon_phase == 3) echo 'First<br>Quarter';
+	if($moon_phase == 4) echo 'Waxing<br>Gibbous';
 	if($moon_phase == 5) echo 'Full';
-	if($moon_phase == 6) echo 'Waning Gibbous';
-	if($moon_phase == 7) echo 'Third Quarter';
-	if($moon_phase == 8) echo 'Waning Crescent';
+	if($moon_phase == 6) echo 'Waning<br>Gibbous';
+	if($moon_phase == 7) echo 'Third<br>Quarter';
+	if($moon_phase == 8) echo 'Waning<br>Crescent';
 	echo '</span>';
 ?></div>
